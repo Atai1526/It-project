1133d47908de13db63776adaf1e30208948be47d
Atai Berdaliev <bboy10@list.ru>
2020-03-27 20:46:07 +0300

    initial

---
+++ js/app.js
@@ -0,0 +1,15 @@
+var swiper1 = new Swiper('.main-slider.swiper-container', {
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    });
+
+ var swiper2 = new Swiper('.prode-slider.swiper-container', {
+      slidesPerView: 4,
+      spaceBetween: 20,
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    });
